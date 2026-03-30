257fb69adeeea3555b3f2220c1f0e8d2fe5f5a19
lotarlmen <val.janushkewitch@yandex.ru>
2026-03-30 16:23:05 +0200

    created Annotated for all usecases

---
+++ src/infrastructure/di.py
@@ -0,0 +1,48 @@
+from fastapi import Depends
+from typing import Annotated
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.infrastructure.db.session import get_async_session
+from src.infrastructure.db.uow import UnitOfWork
+from src.application.usecases.wallet import (
+    DepositUseCase,
+    CreateUseCase,
+    DeleteUseCase,
+    WithdrawUseCase,
+    GetWalletUseCase,
+)
+
+SessionDep = Annotated[AsyncSession, Depends(get_async_session)]
+
+
+def get_uow(session: SessionDep):
+    return UnitOfWork(session)
+
+
+uow_depends = Annotated[UnitOfWork, Depends(get_uow)]
+
+
+async def get_create_wallet_usecase(uow: uow_depends):
+    return CreateUseCase(uow)
+
+
+async def get_deposit_usecase(uow: uow_depends):
+    return DepositUseCase(uow)
+
+
+async def get_delete_usecase(uow: uow_depends):
+    return DeleteUseCase(uow)
+
+
+async def get_withdraw_usecase(uow: uow_depends):
+    return WithdrawUseCase(uow)
+
+
+async def get_wallet_usecase(uow: uow_depends):
+    return GetWalletUseCase(uow)
+
+
+DeleteUsecase = Annotated[DeleteUseCase, Depends(get_delete_usecase)]
+GetUsecase = Annotated[GetWalletUseCase, Depends(get_wallet_usecase)]
+DepositUsecase = Annotated[DepositUseCase, Depends(get_deposit_usecase)]
+WithdrawUsecase = Annotated[WithdrawUseCase, Depends(get_withdraw_usecase)]
+CreateUsecase = Annotated[CreateUseCase, Depends(get_create_wallet_usecase)]
